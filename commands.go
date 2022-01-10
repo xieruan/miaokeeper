9c175c52e2863a6aabb6ffe7779268f71f3e6c07
dl0im <62276692+dl0im@users.noreply.github.com>
2022-01-09 20:29:25 -0500

    feat: locale for cmds

--- commands.go
+++ commands.go
@@ -10,30 +10,30 @@ func CmdWarnUser(m *tb.Message) {
 	gc := GetGroupConfig(m.Chat.ID)
 	if gc != nil && m.ReplyTo != nil {
 		if gc.DisableWarn {
-			SmartSendDelete(m.ReplyTo, "当前群组不允许互相臭嘴哦 ~")
+			SmartSendDelete(m.ReplyTo, Locale("cmd.zc.notAllowed", m.Sender.LanguageCode))
 			return
 		}
 		if m.Sender.ID > 0 && m.Sender.Username != "Channel_Bot" {
 			if m.ReplyTo.Sender.ID == m.Sender.ID {
-				SmartSend(m, "确实")
+				SmartSend(m, Locale("cmd.zc.indeed", m.Sender.LanguageCode))
 			} else if m.ReplyTo.Sender.ID < 0 || m.ReplyTo.Sender.IsBot {
-				SmartSend(m, "我拿它没办法呢 ...")
+				SmartSend(m, Locale("cmd.zc.cantBan", m.Sender.LanguageCode))
 			} else {
 				token := fmt.Sprintf("%d,%d,%d", m.Chat.ID, m.Sender.ID, m.ReplyTo.Sender.ID)
 				limSenderToken := fmt.Sprintf("lim%d,%d,%d", m.Chat.ID, m.Sender.ID)
 				limReciverToken := fmt.Sprintf("lim%d,%d,%d", m.Chat.ID, m.ReplyTo.Sender.ID)
 				if _, ok := zcomap.Get(token); ok {
 					addCredit(m.Chat.ID, m.Sender, -10, true)
-					SmartSend(m, "😠 你自己先漱漱口呢，不要连续臭别人哦！扣 10 分警告一下")
+					SmartSend(m, Locale("cmd.zc.cooldown10", m.Sender.LanguageCode))
 				} else if senderLimit, _ := zcomap.Get(limSenderToken); senderLimit >= 2 {
 					zcomap.Add(limReciverToken)
-					SmartSend(m, "😳 用指令对线是不对的，请大家都冷静下呢～")
+					SmartSend(m, Locale("cmd.zc.cooldown", m.Sender.LanguageCode))
 				} else {
 					zcomap.Add(limSenderToken)
 					zcomap.Add(limReciverToken)
 					zcomap.Set(token, 1)
 					ci := addCredit(m.Chat.ID, m.ReplyTo.Sender, -25, true)
-					SmartSend(m.ReplyTo, fmt.Sprintf("%s, 您被热心的 %s 警告了 ⚠️，请注意管理好自己的行为！暂时扣除 25 分作为警告，如果您的分数低于 -50 分将被直接禁言。若您觉得这是恶意举报，请理性对待，并联系群管理员处理。", GetUserName(m.ReplyTo.Sender), GetUserName(m.Sender)))
+					SmartSend(m.ReplyTo, fmt.Sprintf(Locale("cmd.zc.cooldown", m.Sender.LanguageCode), GetUserName(m.ReplyTo.Sender), GetUserName(m.Sender)))
 					LazyDelete(m)
 					if ci.Credit < -50 {
 						Ban(m.Chat.ID, m.ReplyTo.Sender.ID, 0)
@@ -41,7 +41,7 @@ func CmdWarnUser(m *tb.Message) {
 				}
 			}
 		} else {
-			SmartSend(m, "😠 匿名就不要乱啵啵啦！叭了个叭叭了个叭叭了个叭 ...")
+			SmartSend(m, Locale("cmd.zc.noAnonymous", m.Sender.LanguageCode))
 		}
 	}
 }
@@ -51,30 +51,30 @@ func CmdBanUser(m *tb.Message) {
 		if m.Sender.ID > 0 && m.Sender.Username != "Channel_Bot" {
 			if m.ReplyTo.Sender.ID == m.Sender.ID {
 				if Ban(m.Chat.ID, m.Sender.ID, 1800) == nil {
-					SmartSend(m, "举报自己？那没办法...只好把你 🫒 半小时哦～")
+					SmartSend(m, Locale("cmd.ey.selfReport", m.Sender.LanguageCode))
 					LazyDelete(m.ReplyTo)
 				} else {
-					SmartSend(m, "呜呜呜，封不掉 ～")
+					SmartSend(m, Locale("cmd.ey.notSuccess", m.Sender.LanguageCode))
 				}
 			} else if m.ReplyTo.Sender.IsBot && m.ReplyTo.SenderChat != nil {
 				if m.ReplyTo.SenderChat != nil && m.ReplyTo.SenderChat.ID != m.Chat.ID {
 					if BanChannel(m.Chat.ID, m.ReplyTo.SenderChat.ID) == nil {
-						SmartSend(m, fmt.Sprintf("好的！这就把这个频道封掉啦～ PS: %s 的主人，如果您觉得这是恶意举报，请赶快联系管理员解封哦 ～）", GetChatName(m.ReplyTo.SenderChat)))
+						SmartSend(m, fmt.Sprintf(Locale("cmd.ey.killChannel", m.Sender.LanguageCode), GetChatName(m.ReplyTo.SenderChat)))
 						LazyDelete(m)
 						LazyDelete(m.ReplyTo)
 					} else {
-						SmartSend(m, "呜呜呜，封不掉 ～")
+						SmartSend(m, Locale("cmd.ey.notSuccess", m.Sender.LanguageCode))
 					}
 				} else {
-					SmartSend(m, "叭了个叭叭了个叭叭了个叭 ～")
+					SmartSend(m, Locale("cmd.ey.unexpected", m.Sender.LanguageCode))
 				}
 			} else if m.ReplyTo.Sender.IsBot {
 				if Ban(m.Chat.ID, m.ReplyTo.Sender.ID, 1800) == nil {
-					SmartSend(m, fmt.Sprintf("好的！这就把这个机器人封禁半小时～ PS: %s 的主人，如果您觉得这是恶意举报，请赶快联系管理员解封哦 ～）", GetUserName(m.ReplyTo.Sender)))
+					SmartSend(m, fmt.Sprintf(Locale("cmd.ey.killBot", m.Sender.LanguageCode), GetUserName(m.ReplyTo.Sender)))
 					LazyDelete(m)
 					LazyDelete(m.ReplyTo)
 				} else {
-					SmartSend(m, "呜呜呜，封不掉 ～")
+					SmartSend(m, Locale("cmd.ey.notSuccess", m.Sender.LanguageCode))
 				}
 			} else {
 				userId := m.ReplyTo.Sender.ID
@@ -82,27 +82,27 @@ func CmdBanUser(m *tb.Message) {
 				token := fmt.Sprintf("ad-%d,%d", m.Chat.ID, m.Sender.ID)
 				if zcomap.Add(token) > 3 {
 					addCredit(m.Chat.ID, m.Sender, -5, true)
-					SmartSend(m, "😠 消停一下消停一下，举报太多次啦，扣 5 分缓一缓")
+					SmartSend(m, Locale("cmd.ey.cooldown5", m.Sender.LanguageCode))
 				} else {
 					if _, ok := votemap.Get(vtToken); !ok {
 						if Ban(m.Chat.ID, userId, 1800) == nil {
 							addCredit(m.Chat.ID, m.ReplyTo.Sender, -50, true)
 							addCredit(m.Chat.ID, m.Sender, 15, true)
 							votemap.Set(vtToken, 0)
-							msgTxt := fmt.Sprintf("%s, 您被热心群友 %s 报告有发送恶意言论的嫌疑 ⚠️，请注意自己的发言哦！暂时禁言半小时并扣除 50 分作为警告，举报者 15 分奖励已到账。若您觉得这是恶意举报，可以呼吁小伙伴们公投为您解封（累计满 6 票可以解封并抵消扣分），或者直接联系群管理员处理。", GetUserName(m.ReplyTo.Sender), GetUserName(m.Sender))
+							msgTxt := fmt.Sprintf(Locale("cmd.ey.exec", m.Sender.LanguageCode), GetUserName(m.ReplyTo.Sender), GetUserName(m.Sender))
 							SendBtns(m.ReplyTo, msgTxt, "", GenVMBtns(0, m.Chat.ID, userId, m.Sender.ID))
 							LazyDelete(m)
 							LazyDelete(m.ReplyTo)
 						} else {
-							SmartSend(m, "呜呜呜，封不掉 ～")
+							SmartSend(m, Locale("cmd.ey.notSuccess", m.Sender.LanguageCode))
 						}
 					} else {
-						SmartSend(m, "他已经被检察官带走啦，不要鞭尸啦 ～")
+						SmartSend(m, Locale("cmd.ey.duplicated", m.Sender.LanguageCode))
 					}
 				}
 			}
 		} else {
-			SmartSend(m, "😠 匿名就不要乱啵啵啦！叭了个叭叭了个叭叭了个叭 ...")
+			SmartSend(m, Locale("cmd.zc.noAnonymous", m.Sender.LanguageCode))
 		}
 	}
 }
--- locale.go
+++ locale.go
@@ -1,22 +1,35 @@
 package main
 
-Locale("", m.Sender.LanguageCode)
-
 var LocaleMap = map[string]map[string]string{
 	"default": {
-		"cmd.privateChatFirst": "❌ 请先私聊我然后再运行这个命令哦",
-		"cmd.noGroupPerm": "❌ 您没有权限，亦或是您未再对应群组使用这个命令",
+		"cmd.zc.notAllowed":  "当前群组不允许互相臭嘴哦 ~",
+		"cmd.zc.indeed":      "确实",
+		"cmd.zc.cantBan":     "我拿它没办法呢 ...",
+		"cmd.zc.cooldown10":  "😠 你自己先漱漱口呢，不要连续臭别人哦！扣 10 分警告一下",
+		"cmd.zc.cooldown":    "😳 用指令对线是不对的，请大家都冷静下呢～",
+		"cmd.zc.exec":        "%s, 您被热心的 %s 警告了 ⚠️，请注意管理好自己的行为！暂时扣除 25 分作为警告，如果您的分数低于 -50 分将被直接禁言。若您觉得这是恶意举报，请理性对待，并联系群管理员处理。",
+		"cmd.zc.noAnonymous": "😠 匿名就不要乱啵啵啦！叭了个叭叭了个叭叭了个叭 ...",
 
-		"credit.exportSuccess": "\u200d 导出成功，请在私聊查看结果",
-		"credit.importError": "❌ 无法下载积分备份，请确定您上传的文件格式正确且小于 20MB，大文件请联系管理员手动导入",
+		"cmd.ey.selfReport":  "举报自己？那没办法...只好把你 🫒 半小时哦～",
+		"cmd.ey.notSuccess":  "呜呜呜，封不掉 ～",
+		"cmd.ey.unexpected":  "叭了个叭叭了个叭叭了个叭 ～",
+		"cmd.ey.killChannel": "好的！这就把这个频道封掉啦～ PS: %s 的主人，如果您觉得这是恶意举报，请赶快联系管理员解封哦 ～）",
+		"cmd.ey.killBot":     "好的！这就把这个机器人封禁半小时～ PS: %s 的主人，如果您觉得这是恶意举报，请赶快联系管理员解封哦 ～）",
+		"cmd.ey.cooldown5":   "😠 消停一下消停一下，举报太多次啦，扣 5 分缓一缓",
+		"cmd.ey.exec":        "%s, 您被热心群友 %s 报告有发送恶意言论的嫌疑 ⚠️，请注意自己的发言哦！暂时禁言半小时并扣除 50 分作为警告，举报者 15 分奖励已到账。若您觉得这是恶意举报，可以呼吁小伙伴们公投为您解封（累计满 6 票可以解封并抵消扣分），或者直接联系群管理员处理。",
+		"cmd.ey.duplicated":  "他已经被检察官带走啦，不要鞭尸啦 ～",
+
+		"cmd.privateChatFirst": "❌ 请先私聊我然后再运行这个命令哦",
+		"cmd.noGroupPerm":      "❌ 您没有权限，亦或是您未再对应群组使用这个命令",
+
+		"credit.exportSuccess":    "\u200d 导出成功，请在私聊查看结果",
+		"credit.importError":      "❌ 无法下载积分备份，请确定您上传的文件格式正确且小于 20MB，大文件请联系管理员手动导入",
 		"credit.importParseError": "❌ 解析积分备份错误，请确定您上传的文件格式正确",
 
-		"su.group.addSuccess": "✔️ 已将该组加入积分统计 ～",
+		"su.group.addSuccess":   "✔️ 已将该组加入积分统计 ～",
 		"su.group.addDuplicate": "❌ 该组已经开启积分统计啦 ～",
-		"su.group.delSuccess": "✔️ 已将该组移除积分统计 ～",
+		"su.group.delSuccess":   "✔️ 已将该组移除积分统计 ～",
 		"su.group.delDuplicate": "❌ 该组尚未开启积分统计哦 ～",
-
-
 	},
 	"en": {},
 }
